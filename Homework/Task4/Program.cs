c79f05d0c09fe090cb94cc830551e44fd2b601ba
Kamalov Keldibek <klbest1797@gmail.com>
2023-06-12 17:13:21 +0300

    Desided task 4

--- Homework/Task4/Program.cs
+++ Homework/Task4/Program.cs
@@ -1,2 +1,17 @@
-﻿// Напишите программу
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу , которая принимает на вход три числа и выдает максимальное из эти чисел.
+Console.WriteLine("Input first number");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int second_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number");
+int third_number = Convert.ToInt32(Console.ReadLine());
+int max = first_number;
+if (second_number > max)
+{
+    max = second_number;
+}
+if (third_number > max)
+{
+    max = third_number;
+}
+Console.WriteLine(max);
