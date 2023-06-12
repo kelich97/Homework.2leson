b53d033c277f453f47d4e1bbe91ac2f44b003e6e
Kamalov Keldibek <klbest1797@gmail.com>
2023-06-12 17:21:43 +0300

    Desided task6

---
+++ Homework/Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу , которая на вход принимает число и выдает , являеться ли число чётным .
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Не чётное");
+}
